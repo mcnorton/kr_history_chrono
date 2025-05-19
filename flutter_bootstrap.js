e04092864ce1b170bc5ab66751c44590b20e9f65
mcnorton <mcnorton@mnMBaM1.local>
2025-05-19 16:51:29 +0900

    fix. textstyle fonts

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3794789281"
+    serviceWorkerVersion: "1729772781"
   }
 });
